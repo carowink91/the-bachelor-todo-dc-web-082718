e27f0c74e1e75b845a45aa5c8959dfdce78e03b4
angelica winkler <caw400@nyu.edu>
2018-08-08 14:02:24 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,13 +48,13 @@ end
 
 
 def count_contestants_by_hometown(data, hometown)
-
-end
-
-def get_occupation(data, hometown)
   fromHomeTown = []
 
   binding.pry
+end
+
+def get_occupation(data, hometown)
+
 end
 
 def get_average_age_for_season(data, season)
