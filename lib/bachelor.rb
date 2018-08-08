06aa2556fefa81ba99c693db51c5fccb0d455fa2
angelica winkler <caw400@nyu.edu>
2018-08-08 14:11:31 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -54,7 +54,7 @@ def count_contestants_by_hometown(data, hometown)
     contestants.each do |personalHash, pairs|
       personalHash.each do |question, answer|
         if question == "occupation"
-          answer.each do |answer|
+          answer.each do |town|
             binding.pry
             if answer == hometown
               fromHomeTown << hometown
